4f61c37be1ff955197690c3270a3a6e7f1f14c05
CiccioBro17 <203853964+CiccioBro17@users.noreply.github.com>
2025-08-13 14:49:53 +0200

    Create simracingtoolbox.asm and simracingtoolbox.bin

---
+++ examples/simracingtoolbox.asm
@@ -0,0 +1,232 @@
+; example: print "Join the SimRacing Toolbox' discord server and become a tester to Minghe's (25.9k subs) app‼️ discord.gg/zAUAVGK7" and halt
+        LDI A, 'J'
+        OUT A
+        LDI A, 'o'
+        OUT A
+        LDI A, 'i'
+        OUT A
+        LDI A, 'n'
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, 't'
+        OUT A
+        LDI A, 'h'
+        OUT A
+        LDI A, 'e'
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, 'S'
+        OUT A
+        LDI A, 'i'
+        OUT A
+        LDI A, 'm'
+        OUT A
+        LDI A, 'R'
+        OUT A
+        LDI A, 'a'
+        OUT A
+        LDI A, 'c'
+        OUT A
+        LDI A, 'i'
+        OUT A
+        LDI A, 'n'
+        OUT A
+        LDI A, 'g'
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, 'T'
+        OUT A
+        LDI A, 'o'
+        OUT A
+        LDI A, 'o'
+        OUT A
+        LDI A, 'l'
+        OUT A
+        LDI A, 'b'
+        OUT A
+        LDI A, 'o'
+        OUT A
+        LDI A, 'x'
+        OUT A
+        LDI A, '''
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, 'd'
+        OUT A
+        LDI A, 'i'
+        OUT A
+        LDI A, 's'
+        OUT A
+        LDI A, 'c'
+        OUT A
+        LDI A, 'o'
+        OUT A
+        LDI A, 'r'
+        OUT A
+        LDI A, 'd'
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, 's'
+        OUT A
+        LDI A, 'e'
+        OUT A
+        LDI A, 'r'
+        OUT A
+        LDI A, 'v'
+        OUT A
+        LDI A, 'e'
+        OUT A
+        LDI A, 'r'
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, 'a'
+        OUT A
+        LDI A, 'n'
+        OUT A
+        LDI A, 'd'
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, 'b'
+        OUT A
+        LDI A, 'e'
+        OUT A
+        LDI A, 'c'
+        OUT A
+        LDI A, 'o'
+        OUT A
+        LDI A, 'm'
+        OUT A
+        LDI A, 'e'
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, 'a'
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, 't'
+        OUT A
+        LDI A, 'e'
+        OUT A
+        LDI A, 's'
+        OUT A
+        LDI A, 't'
+        OUT A
+        LDI A, 'e'
+        OUT A
+        LDI A, 'r'
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, 't'
+        OUT A
+        LDI A, 'o'
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, 'M'
+        OUT A
+        LDI A, 'i'
+        OUT A
+        LDI A, 'n'
+        OUT A
+        LDI A, 'g'
+        OUT A
+        LDI A, 'h'
+        OUT A
+        LDI A, 'e'
+        OUT A
+        LDI A, '''
+        OUT A
+        LDI A, 's'
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, '('
+        OUT A
+        LDI A, '2'
+        OUT A
+        LDI A, '5'
+        OUT A
+        LDI A, '.'
+        OUT A
+        LDI A, '9'
+        OUT A
+        LDI A, 'k'
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, 's'
+        OUT A
+        LDI A, 'u'
+        OUT A
+        LDI A, 'b'
+        OUT A
+        LDI A, 's'
+        OUT A
+        LDI A, ')'
+        OUT A
+        LDI A, ' '
+        OUT A
+        LDI A, 'a'
+        OUT A
+        LDI A, 'p'
+        OUT A
+        LDI A, 'p'
+        OUT A
+        LDI A, '!'
+        OUT A
+        LDI A, '!'
+        OUT A
+        LDI A, 10 
+        ; newline
+        OUT A
+        LDI A, 'd'
+        OUT A
+        LDI A, 'i'
+        OUT A
+        LDI A, 's'
+        OUT A
+        LDI A, 'c'
+        OUT A
+        LDI A, 'o'
+        OUT A
+        LDI A, 'r'
+        OUT A
+        LDI A, 'd'
+        OUT A
+        LDI A, '.'
+        OUT A
+        LDI A, 'g'
+        OUT A
+        LDI A, 'g'
+        OUT A
+        LDI A, '/'
+        OUT A
+        LDI A, 'z'
+        OUT A
+        LDI A, 'A'
+        OUT A
+        LDI A, 'U'
+        OUT A
+        LDI A, 'A'
+        OUT A
+        LDI A, 'V'
+        OUT A
+        LDI A, 'G'
+        OUT A
+        LDI A, 'K'
+        OUT A
+        LDI A, '7'
+        OUT A
+        LDI A, 10 
+        ; newline
+        OUT A
+        HLT
